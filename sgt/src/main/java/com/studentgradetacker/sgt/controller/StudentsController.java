ea2ae149cbb4535de950beea59023b98715cf41d
elizajsampan <elizajsampan0319@gmail.com>
2024-07-11 20:35:00 +0800

    added getmapping for students

--- sgt/src/main/java/com/studentgradetacker/sgt/controller/StudentsController.java
+++ sgt/src/main/java/com/studentgradetacker/sgt/controller/StudentsController.java
@@ -1,4 +1,32 @@
 package com.studentgradetacker.sgt.controller;
 
+import com.studentgradetacker.sgt.model.Students;
+import com.studentgradetacker.sgt.respository.StudentsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sgt")
 public class StudentsController {
+
+    @Autowired
+    StudentsRepository studentsRepository;
+
+    @GetMapping("/students")
+   List<Students> getAllStudents() {
+        System.out.println(studentsRepository.findAll());
+        return studentsRepository.findAll();
+    };
+
+    @GetMapping("/students/{id}")
+    Students getStudentByStudentId(@PathVariable Integer id) {
+        System.out.println(studentsRepository.findByStudentId(id));
+        return studentsRepository.findByStudentId(id);
+    }
+
 }
--- sgt/src/main/java/com/studentgradetacker/sgt/respository/StudentsRepository.java
+++ sgt/src/main/java/com/studentgradetacker/sgt/respository/StudentsRepository.java
@@ -2,6 +2,15 @@ package com.studentgradetacker.sgt.respository;
 
 import com.studentgradetacker.sgt.model.Students;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StudentsRepository extends JpaRepository<Students, Integer> {
+
+    List<Students> findAll();
+
+    Students findByStudentId(Integer studentId);
+
 }
